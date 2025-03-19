58604e882646e376fd702faadd92327d2032604d
sara alila <saraalila@Sofias-MacBook-Pro.local>
2025-03-19 17:27:34 +0000

    chore: add migration runner script

---
+++ migrate.py
@@ -0,0 +1,11 @@
+from alembic.config import Config
+from alembic import command
+
+
+def run_migrations():
+    alembic_cfg = Config("../app/alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+
+if __name__ == "__main__":
+    run_migrations()
